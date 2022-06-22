4366eb142fcecd8551394abe79c273bdf2c94be6
Alexander Trost <galexrt@googlemail.com>
2022-06-22 14:39:04 +0200

    code: use getopts for posix compliant flag parsing
    Split each info gathering step into separate functions to make it easier
    to group them.
    Introduce cluster and operator namespace flags.
    
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Script to gather debug information for troubleshooting a rook-ceph cluster.
 
-It collects the following information : 
-1. All the pod logs from rook-ceph namespace in individual files.
+It collects the following information:
+
+1. All the pod logs from Rook Ceph cluster and if separate operator namespace in individual files.
 2. Ceph command outputs for debugging the current state of the cluster.
 3. Output of `# kubectl version`
 4. All the Deployment YAMLs : `kubectl get -n rook-ceph deployments --output yaml`
@@ -9,9 +10,12 @@ It collects the following information :
 6. Node related information : `kubectl get nodes` and `kubectl get nodes -oyaml`
 7. rook-config-override ConfigMap : `kubectl -n rook-ceph get ConfigMap rook-config-override -o yaml`
 
-**Pre-requisites: Toolbox pod must be running to collect some of the information needed by the debug script**  
+**Pre-requisites: Toolbox pod must be running to collect some of the information needed by the debug script**
 
-Usage : `sh gather-logs.sh <-D>`  
-`-D` : Optional command line argument to crank up the debug logging for rook-ceph cluster.
+Usage : `sh gather-logs.sh <-hd>`
+* `-h`: Show help menu.
+* `-d` : Optional command line argument to crank up the debug logging for rook-ceph cluster.
+* `-n CLUSTER_NAMESPACE`: Optional command line argument to set the rook-ceph cluster namespace.
+* `-o OPERATOR_NAMESPACE`: Optional command line argument to set the rook-ceph-operator namespace.
 
-The script creates `debug-data` directory containing all the debug information in `/tmp/` directory of the system.
+The script creates a temp directory on the system prefixed with `gather-logs-`.
--- gather-logs.sh
+++ gather-logs.sh
@@ -1,57 +1,118 @@
-#!/bin/bash
+#!/bin/sh
 
-mkdir /tmp/debug-data
-cd /tmp/debug-data
-mkdir logs
-mkdir ceph-commands
+CLUSTER_NAMESPACE="${CLUSTER_NAMESPACE:-rook-ceph}"
+OPERATOR_NAMESPACE="${OPERATOR_NAMESPACE:-}"
 
-if [ $1 = "-D" ]
-then
-kubectl -n rook-ceph get configmaps rook-ceph-operator-config -oyaml > op-configmap
-sed -i '4i\ \ CSI_LOG_LEVEL: "5"' op-configmap
-sed -i "s/^  ROOK_LOG_LEVEL: INFO/  ROOK_LOG_LEVEL: DEBUG/" op-configmap
-kubectl apply -f op-configmap
-fi
+gatherKubernetesPodLogs() {
+    mkdir -p logs
 
-kubectl version > kubectl_version
-kubectl get nodes > kubectl_get-nodes
-kubectl get nodes -oyaml > kubectl_get-nodes-yaml
-
-kubectl -n rook-ceph get all > rook_ceph-get-all
-
-for p in $(kubectl -n rook-ceph get pods -o jsonpath='{.items[*].metadata.name}')
-do
-    for c in $(kubectl -n rook-ceph get pod ${p} -o jsonpath='{.spec.containers[*].name}')
-    do
-        echo "BEGIN logs from pod: ${p} ${c}"
-        kubectl -n rook-ceph logs -c ${c} ${p} > logs/${p}_${c}
-        echo "END logs from pod: ${p} ${c}"
+    for p in $(kubectl -n "${CLUSTER_NAMESPACE}" get pods -o jsonpath='{.items[*].metadata.name}'); do
+        for c in $(kubectl -n "${CLUSTER_NAMESPACE}" get pod "${p}" -o jsonpath='{.spec.containers[*].name}'); do
+            echo "BEGIN logs from pod: ${p} ${c}"
+            kubectl -n "${CLUSTER_NAMESPACE}" logs -c "${c}" "${p}" > "logs/${p}-${c}"
+            echo "END logs from pod: ${p} ${c}"
+        done
     done
+}
+
+gatherKubernetesObjects() {
+    # shellcheck disable=SC2094
+    kubectl get -n "${CLUSTER_NAMESPACE}" deployments --output yaml > deployments
+    # Gather info from operator namespace if it is run separately
+    [ "${CLUSTER_NAMESPACE}" != "${OPERATOR_NAMESPACE}" ] && \
+        kubectl get -n "${OPERATOR_NAMESPACE}" deployments --output yaml > deployments-operator
+
+    kubectl -n "${CLUSTER_NAMESPACE}" get configmap --output yaml > configmaps
+    [ "${CLUSTER_NAMESPACE}" != "${OPERATOR_NAMESPACE}" ] && \
+        kubectl -n "${OPERATOR_NAMESPACE}" get configmap --output yaml > configmaps
+
+    kubectl -n "${CLUSTER_NAMESPACE}" get configmaps rook-config-override --output yaml > configmap_rook-config-override
+
+    kubectl version > kubectl_version
+    kubectl get nodes > kubectl_get-nodes
+    kubectl get nodes --output yaml > kubectl_get-nodes-yaml
+
+    # "all" is not "all" but it contains most of the other basic K8S resources we might be interested in
+    kubectl -n "${CLUSTER_NAMESPACE}" get all > rook_ceph-get-all
+}
+
+gatherCephCommands() {
+    mkdir -p ceph-commands
+
+    # Ceph command outputs
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph -s > ceph-commands/ceph_status 
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph df > ceph-commands/ceph_df
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph osd df tree > ceph-commands/ceph_osd_df_tree
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph health detail > ceph-commands/ceph_health_detail
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph df detail > ceph-commands/ceph_df_detail
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph osd tree > ceph-commands/ceph_osd_tree
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph osd dump > ceph-commands/ceph_osd_dump
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph osd perf > ceph-commands/ceph_osd_perf
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph osd pool ls detail > ceph-commands/ceph_osd_pool_ls_detail
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph osd pool autoscale-status > ceph-commands/ceph_osd_pool_autoscale_status
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph osd numa-status > ceph-commands/ceph_osd_numa-status
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph osd blocked-by > ceph-commands/ceph_osd_blocked-by
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph mon dump > ceph-commands/ceph_mon_dump
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph mon stat > ceph-commands/ceph_mon_stat
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph pg stat > ceph-commands/ceph_pg_stat
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph pg dump > ceph-commands/ceph_pg_dump
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph fs ls > ceph-commands/ceph_fs_ls
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph fs dump > ceph-commands/ceph_fs_dump
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph mds stat > ceph-commands/ceph_mds_stat
+    kubectl -n "${CLUSTER_NAMESPACE}" exec -it deploy/rook-ceph-tools -- ceph time-sync-status > ceph-commands/ceph_time_sync_status
+}
+
+enableDebugLog() {
+    kubectl -n "${OPERATOR_NAMESPACE}" patch configmaps/rook-ceph-operator-config \
+        --type merge \
+        -p '{"data":{"CSI_LOG_LEVEL":"5", "ROOK_LOG_LEVEL": "DEBUG"}}'
+}
+
+showHelp() {
+    echo ""
+}
+
+LOGS_TMP_DIR="$(mktemp -d -t gather-logs-XXXXXXXXXX)"
+
+cd "${LOGS_TMP_DIR}" || { echo "Failed to cd to ${LOGS_TMP_DIR} dir."; exit 1; }
+
+# Flag Parsing BEGIN
+# Reset getopts index
+OPTIND=1
+
+# Initialize settings
+enable_debug_log=0
+
+while getopts "h?dno:" opt; do
+  case "$opt" in
+    h|\?)
+        showHelp
+        exit 0
+        ;;
+    d)
+        enable_debug_log=1
+        ;;
+    n)
+        CLUSTER_NAMESPACE="${OPTARG}"
+        ;;
+    o)
+        OPERATOR_NAMESPACE="${OPTARG}"
+        ;;
+  esac
 done
 
-kubectl get -n rook-ceph deployments --output yaml > deployments
+shift $(( OPTIND - 1 ))
 
-kubectl get configmap -n rook-ceph -oyaml > configmaps
-kubectl -n rook-ceph get ConfigMap rook-config-override -o yaml > configmap_rook-config-override
+[ "${1:-}" = "--" ] && shift
+# Flag Parsing END
 
-# Ceph command outputs
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph -s > ceph-commands/ceph_status 
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph df > ceph-commands/ceph_df
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph osd df tree > ceph-commands/ceph_osd_df_tree
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph health detail > ceph-commands/ceph_health_detail
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph df detail > ceph-commands/ceph_df_detail
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph osd tree > ceph-commands/ceph_osd_tree
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph osd dump > ceph-commands/ceph_osd_dump
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph osd perf > ceph-commands/ceph_osd_perf
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph osd pool ls detail > ceph-commands/ceph_osd_pool_ls_detail
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph osd pool autoscale-status > ceph-commands/ceph_osd_pool_autoscale_status
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph osd numa-status > ceph-commands/ceph_osd_numa-status
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph osd blocked-by > ceph-commands/ceph_osd_blocked-by
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph mon dump > ceph-commands/ceph_mon_dump
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph mon stat > ceph-commands/ceph_mon_stat
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph pg stat > ceph-commands/ceph_pg_stat
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph pg dump > ceph-commands/ceph_pg_dump
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph fs ls > ceph-commands/ceph_fs_ls
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph fs dump > ceph-commands/ceph_fs_dump
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph mds stat > ceph-commands/ceph_mds_stat
-kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- ceph time-sync-status > ceph-commands/ceph_time_sync_status
+# Set the operator namespace to the cluster namespace if it is still empty after the flag parsing
+[ -z "${OPERATOR_NAMESPACE}" ] && OPERATOR_NAMESPACE="${CLUSTER_NAMESPACE}"
+
+if [ ${enable_debug_log} = 1 ]; then
+    enableDebugLog
+fi
+
+gatherKubernetesPodLogs
+gatherKubernetesObjects
+gatherCephCommands
